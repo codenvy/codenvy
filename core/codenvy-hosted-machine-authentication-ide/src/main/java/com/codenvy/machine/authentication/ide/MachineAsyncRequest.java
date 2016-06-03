db6e50fba0fbb1ead66ae5cdf9e0154499c36a85
Anton Korneta <akorneta@codenvy.com>
2016-06-03 15:11:09 +0300

    fix machine request send() method

--- core/codenvy-hosted-machine-authentication-ide/src/main/java/com/codenvy/machine/authentication/ide/MachineAsyncRequest.java
+++ core/codenvy-hosted-machine-authentication-ide/src/main/java/com/codenvy/machine/authentication/ide/MachineAsyncRequest.java
@@ -60,7 +60,19 @@ public class MachineAsyncRequest extends AsyncRequest {
                     @Override
                     public void apply(String machine) throws OperationException {
                         MachineAsyncRequest.this.header(AUTHORIZATION, machine);
-                        resolve.apply(null);
+                        MachineAsyncRequest.super.send()
+                                                 .then(new Operation<Void>() {
+                                                     @Override
+                                                     public void apply(Void arg) throws OperationException {
+                                                         resolve.apply(null);
+                                                     }
+                                                 })
+                                                 .catchError(new Operation<PromiseError>() {
+                                                     @Override
+                                                     public void apply(PromiseError arg) throws OperationException {
+                                                         reject.apply(arg);
+                                                     }
+                                                 });
                     }
                 }).catchError(new Operation<PromiseError>() {
                     @Override
