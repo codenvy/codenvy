b61e3ddcda405d96b4cfc3841537bb17c982dbe5
Max Shaposhnik <mshaposhnik@codenvy.com>
2016-06-08 14:27:40 +0300

    fix imports DashboardRedirectionFilter

--- assembly/onpremises-ide-packaging-war-ide/src/main/java/com/codenvy/onpremises/DashboardRedirectionFilter.java
+++ assembly/onpremises-ide-packaging-war-ide/src/main/java/com/codenvy/onpremises/DashboardRedirectionFilter.java
@@ -15,7 +15,12 @@
 package com.codenvy.onpremises;
 
 import javax.inject.Singleton;
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
