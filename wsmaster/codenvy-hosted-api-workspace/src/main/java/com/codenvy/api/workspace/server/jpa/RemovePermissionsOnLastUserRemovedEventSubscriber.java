41735a23ac4c781c6a9b8a7921eee4b41052755e
Max Shaposhnik <mshaposhnik@codenvy.com>
2016-09-05 12:30:54 +0300

    Code fixes

--- wsmaster/codenvy-hosted-api-workspace/src/main/java/com/codenvy/api/workspace/server/jpa/RemovePermissionsOnLastUserRemovedEventSubscriber.java
+++ wsmaster/codenvy-hosted-api-workspace/src/main/java/com/codenvy/api/workspace/server/jpa/RemovePermissionsOnLastUserRemovedEventSubscriber.java
@@ -33,7 +33,6 @@ import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
