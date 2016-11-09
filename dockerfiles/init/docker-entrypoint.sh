63d65f93360c269841af9466e4f75767db0491b4
Roman Iuvshin <riuvshin@codenvy.com>
2016-11-09 20:58:25 +0200

    fix init image build

--- dockerfiles/init/docker-entrypoint.sh
+++ dockerfiles/init/docker-entrypoint.sh
@@ -3,8 +3,7 @@
 # Make sure service is running
 cp -rf /files/manifests /copy
 cp -rf /files/modules /copy
-cp -rf /files/README.md /copy
-cp -rf /files/DOCKERIZED.md /copy
+cp -rf /files/docs /copy
 # do not copy codenvy.env if exist
 if [ ! -f  /copy/codenvy.env ]; then
     cp /files/codenvy.env /copy
