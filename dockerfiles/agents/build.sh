0e1a9d338ef7e23fbfaf41dd8c2e20891520fb01
Anatoliy Bazko <abazko@codenvy.com>
2016-12-14 10:41:04 +0200

    Fix up

--- dockerfiles/agents/build.sh
+++ dockerfiles/agents/build.sh
@@ -5,12 +5,35 @@
 # which accompanies this distribution, and is available at
 # http://www.eclipse.org/legal/epl-v10.html
 
-IMAGE_NAME="eclipse/che-agents"
+IMAGE_NAME="codenvy/agents"
 . $(cd "$(dirname "$0")"; pwd)/../build.include
+
+# prepare
+DIR=$(cd "$(dirname "$0")"; pwd)
+DEPENDENCY_DIR="${DIR}/dependency"
+POM_VERSION=$(cat ${DIR}/../../pom.xml | grep "^        <version>.*</version>$" | awk -F'[><]' '{print $3}')
+
+rm -rf ${DEPENDENCY_DIR}
+
+TERMINAL_AGENTS=("org.eclipse.che:exec-agent:${POM_VERSION}:tar.gz:linux_amd64"
+                 "org.eclipse.che:exec-agent:${POM_VERSION}:tar.gz:linux_arm7");
+
+for artifact in ${TERMINAL_AGENTS[@]}; do
+    prefix=$(echo ${artifact} | cut -d ':' -f 5)
+    mvn dependency:copy -Dartifact=${artifact} -DoutputDirectory=${DEPENDENCY_DIR}/${prefix}/terminal -Dmdep.stripVersion=true
+    mv ${DEPENDENCY_DIR}/${prefix}/terminal/exec-agent-${prefix}.tar.gz ${DEPENDENCY_DIR}/${prefix}/terminal/websocket-terminal-${prefix}.tar.gz
+done
+
+mvn dependency:copy -Dartifact=com.codenvy.onpremises:onpremises-ide-packaging-tomcat-ext-server:${POM_VERSION}:tar.gz \
+                    -DoutputDirectory=${DEPENDENCY_DIR} \
+                    -Dmdep.stripVersion=true
+
+mv ${DEPENDENCY_DIR}/onpremises-ide-packaging-tomcat-ext-server.tar.gz ${DEPENDENCY_DIR}/ws-agent.tar.gz
 
 # build
 init
 build
 
-
+# clean up
+rm -rf ${DEPENDENCY_DIR}
 
