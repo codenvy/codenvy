37fb5b42aadbcb2514c3a1e1ee71fdeb0af31b24
Sergii Leschenko <sleschenko@codenvy.com>
2017-02-27 17:41:50 +0200

    Make ResourceUsageTracker to return model interface instead of implementation

--- wsmaster/codenvy-hosted-api-resource/src/test/java/com/codenvy/resource/api/usage/tracker/RuntimeResourceUsageTrackerTest.java
+++ wsmaster/codenvy-hosted-api-resource/src/test/java/com/codenvy/resource/api/usage/tracker/RuntimeResourceUsageTrackerTest.java
@@ -15,7 +15,7 @@
 package com.codenvy.resource.api.usage.tracker;
 
 import com.codenvy.resource.api.type.RuntimeResourceType;
-import com.codenvy.resource.spi.impl.ResourceImpl;
+import com.codenvy.resource.model.Resource;
 
 import org.eclipse.che.account.api.AccountManager;
 import org.eclipse.che.account.shared.model.Account;
@@ -86,7 +86,7 @@ public class RuntimeResourceUsageTrackerTest {
         when(workspaceManager.getByNamespace(anyString(), anyBoolean()))
                 .thenReturn(singletonList(createWorkspace(WorkspaceStatus.STOPPED)));
 
-        Optional<ResourceImpl> usedRuntimesOpt = runtimeResourceUsageTracker.getUsedResource("account123");
+        Optional<Resource> usedRuntimesOpt = runtimeResourceUsageTracker.getUsedResource("account123");
 
         assertFalse(usedRuntimesOpt.isPresent());
     }
@@ -101,10 +101,10 @@ public class RuntimeResourceUsageTrackerTest {
                                   .map(RuntimeResourceUsageTrackerTest::createWorkspace)
                                   .collect(Collectors.toList()));
 
-        Optional<ResourceImpl> usedRuntimesOpt = runtimeResourceUsageTracker.getUsedResource("account123");
+        Optional<Resource> usedRuntimesOpt = runtimeResourceUsageTracker.getUsedResource("account123");
 
         assertTrue(usedRuntimesOpt.isPresent());
-        ResourceImpl usedRuntimes = usedRuntimesOpt.get();
+        Resource usedRuntimes = usedRuntimesOpt.get();
         assertEquals(usedRuntimes.getType(), RuntimeResourceType.ID);
         assertEquals(usedRuntimes.getAmount(), WorkspaceStatus.values().length - 1); //except stopped workspaces
         assertEquals(usedRuntimes.getUnit(), RuntimeResourceType.UNIT);
