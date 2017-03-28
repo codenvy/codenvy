3d2d46c05933ab39c86b3f4f8c45fb6dc9c9bf28
Mihail Kuznyetsov <mkuznyetsov@codenvy.com>
2017-03-28 15:46:24 +0300

    fix migration

--- wsmaster/codenvy-sql-schema/src/main/resources/codenvy-schema/5.6.0/postgresql/2.1__make_factory_names_unique.sql
+++ wsmaster/codenvy-sql-schema/src/main/resources/codenvy-schema/5.6.0/postgresql/2.1__make_factory_names_unique.sql
@@ -21,18 +21,18 @@ SET name = concat('f', right(id, 9)) WHERE name IS NULL OR name = '';
 -- Make names unique for the same user, e.g if there is more than one factory with same name and user,
 -- leave first one and rename others.
 WITH dupes AS
-  ( SELECT id, user_id, name
+  ( SELECT id, userid, name
     FROM factory
-    WHERE (name,user_id)
-    IN (SELECT name, user_id
+    WHERE (name,userid)
+    IN (SELECT name, userid
       FROM factory
-      GROUP BY name, user_id
+      GROUP BY name, userid
       HAVING count(*) > 1)
   ),
   uniques AS
   ( WITH q as
     ( SELECT *, row_number()
-      OVER (PARTITION BY name,user_id ORDER BY name,user_id)
+      OVER (PARTITION BY name,userid ORDER BY name,userid)
       AS rn FROM factory
     )
     SELECT id FROM q WHERE rn = 1
