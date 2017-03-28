99923930d690274edf85eb339a9d90d9c8796a63
Roman Iuvshin <riuvshin@codenvy.com>
2017-03-28 18:10:07 +0300

    fixup

--- dockerfiles/swarm/entrypoint.sh
+++ dockerfiles/swarm/entrypoint.sh
@@ -13,4 +13,12 @@ if [ ! -d  /var/log/swarm ]; then
     mkdir -p /var/log/swarm
 fi
 
-exec /swarm manage -H 0.0.0.0:2375 file:///node_list >>/var/log/swarm/swarm.log 2>&1
+exec /swarm manage -H 0.0.0.0:2375 file:///node_list 2>&1 | tee -a /var/log/swarm/swarm.log &
+
+pid=`pidof swarm`
+trap "echo 'Stopping PID $pid'; kill -SIGTERM $pid" SIGINT SIGTERM
+# A signal emitted while waiting will make the wait command return code > 128
+# Let's wrap it in a loop that doesn't end before the process is indeed stopped
+while kill -0 $pid > /dev/null 2>&1; do
+    wait
+done
